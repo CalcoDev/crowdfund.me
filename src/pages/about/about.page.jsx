4cd458b84fed655e9182c2470d3cb2b14152321b
Calco Dev <calcopoddev@gmail.com>
2023-05-21 01:37:14 +0300

    Animated about page.

--- src/pages/about/about.page.jsx
+++ src/pages/about/about.page.jsx
@@ -6,7 +6,7 @@ const About = () => {
     return (
         <div className="bg-cfm-light-grey">
             <AnimationLoader />
-            <Navigation className="" />
+            <Navigation className="cfm-enter-top cfm-delay-2 once" />
 
             <section
                 className="
@@ -14,16 +14,16 @@ const About = () => {
                     grid grid-cols-9 gap-2 grid-rows-9
                 "
             >
-                <div className="col-start-3 col-end-8 row-start-[2] row-end-[4] flex flex-col justify-center items-center">
+                <div className="cfm-enter-top once col-start-3 col-end-8 row-start-[2] row-end-[4] flex flex-col justify-center items-center">
                     <h1 className="cfm-header text-center">ABOUT US</h1>
                     <hr className="text-cfm-white border-2 rounded-md w-1/5 max-w-[2rem] mx-auto" />
                 </div>
 
-                <div className="col-start-2 col-end-5 row-start-[4] row-end-[9] flex justify-center items-center">
+                <div className="cfm-enter-left cfm-delay-1 once col-start-2 col-end-5 row-start-[3] row-end-[9] flex justify-center items-center">
                     <BnWImage src="/images/about/founders.png" alt="Founders" />
                 </div>
 
-                <p className="col-start-6 col-end-9 row-start-[4] row-end-[9] cfm-subheader text-sm flex justify-center items-center">
+                <p className="cfm-enter-right cfm-delay-1 once col-start-6 col-end-9 row-start-[3] row-end-[9] cfm-subheader text-sm flex justify-center items-center">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Aliquam nisl metus, viverra id rutrum et, facilisis eget
                     ligula. Mauris interdum viverra suscipit. Nullam ac lectus
@@ -34,9 +34,24 @@ const About = () => {
                     volutpat orci, vitae viverra neque ligula condimentum risus.
                     Sed urna leo, blandit at laoreet et, bibendum in odio.
                 </p>
+
+                <img
+                    onClick={(e) => {
+                        e.preventDefault()
+                        document.getElementById('secondary').scrollIntoView()
+                    }}
+                    className="
+                        cfm-enter-bottom once cfm-delay-3
+                        transition duration-300 ease-in-out 
+                        transform absolute bottom-8 left-1/2 -translate-x-1/2
+                        hover:scale-125 hover:cursor-pointer active:scale-90
+                    "
+                    src="/misc/arrow.svg"
+                />
             </section>
 
             <section
+                id="secondary"
                 className={`
                     h-screen-fill
                     bg-cfm-dark-grey
@@ -44,7 +59,7 @@ const About = () => {
                     items-center
                 `}
             >
-                <p className="col-start-2 col-end-5 cfm-subheader text-sm flex justify-center items-center">
+                <p className="cfm-enter-left once col-start-2 col-end-5 cfm-subheader text-sm flex justify-center items-center">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Aliquam nisl metus, viverra id rutrum et, facilisis eget
                     ligula. Mauris interdum viverra suscipit. Nullam ac lectus
@@ -56,7 +71,7 @@ const About = () => {
                     Sed urna leo, blandit at laoreet et, bibendum in odio.
                 </p>
 
-                <div className="col-start-6 col-end-9">
+                <div className="cfm-enter-right once col-start-6 col-end-9">
                     <BnWImage
                         src="/images/about/crowdfundme.png"
                         alt="Founders"
